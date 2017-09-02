6d4b9e5b4512750aa9a6910f4334bad17242340d
Shen <1306980900@qq.com>
2017-09-03 01:52:13 +0800

    Update merge.js

--- build/merge.js
+++ build/merge.js
@@ -9,7 +9,7 @@ args.forEach(function (val, index, array) {
     console.log(`${index}: ${val}`);
 });
 
-let moduleStr = fs.readFileSync(path.resolve(process.cwd(), './temp/mine.js'), 'utf8').replace('global.$', '$');
+let moduleStr = fs.readFileSync(path.resolve(process.cwd(), './temp/stat.js'), 'utf8').replace('global.$', '$');
 if (uglify) {
     moduleStr = uglifyjs.minify(moduleStr, {
         mangle: {
@@ -18,7 +18,7 @@ if (uglify) {
     }).code;
 }
 
-let outputFile = args[1] || './dist/mine.js';
+let outputFile = args[1] || './dist/stat.js';
 let option = args[0] || 'jquery-in';
 let jQueryOrZepto = option.indexOf('jquery') >= 0 ? 'jquery' : 'zepto';
 let jz = (option.indexOf('-in') < 0) ? '' : fs.readFileSync(path.resolve(process.cwd(), `./src/lib/${jQueryOrZepto}.min.js`), 'utf8');
